d3f420e5254bf7d07921821cc0fe18c4f49c0e2e
Tony Colston <tonetheman@gmail.com>
2017-04-16 15:38:26 -0500

    added two starting examples

---
+++ start_1.py
@@ -0,0 +1,9 @@
+
+import turtle
+
+bob = turtle.Turtle()
+bob.forward(100)
+
+turtle.done()
+
+
---
+++ start_2.py
@@ -0,0 +1,20 @@
+
+import turtle
+
+bob = turtle.Turtle()
+
+# move forward
+bob.forward(100)
+
+# turn right by 90 degrees
+bob.right(90)
+
+# repeat
+bob.forward(100)
+bob.right(90)
+bob.forward(100)
+bob.right(90)
+
+turtle.done()
+
+
